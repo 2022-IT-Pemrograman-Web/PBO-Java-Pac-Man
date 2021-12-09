1fbc490f350500ec65bb2e4606f4812a78304177
Alffaza <ridzkiraihanalfaza@gmail.com>
2021-12-09 23:54:40 +0700

    add custom fonts

---
+++ pacman/src/pacman/FontLoader.java
@@ -0,0 +1,23 @@
+package pacman;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class FontLoader {
+    public static Font getFontFromFile(String fontname, float fontsize) {
+        Font customFont = new Font("Arial",Font.PLAIN,0);
+        try {
+            //create the font to use. Specify the size!
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("res/fonts/" + fontname + ".ttf")).deriveFont(fontsize);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            //register the font
+            ge.registerFont(customFont);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(FontFormatException e) {
+            e.printStackTrace();
+        }
+        return customFont;
+    }
+}
