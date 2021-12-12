ad595e98f1384b09fea7aec874b7a20e9da4721b
EvanZJ <89305726+EvanZJ@users.noreply.github.com>
2021-12-12 19:08:45 +0700

    adding loopable music option

--- pacman/src/pacman/SoundPlayer.java
+++ pacman/src/pacman/SoundPlayer.java
@@ -28,4 +28,26 @@ public class SoundPlayer {
             }
         }).start();
     }
+    public static synchronized void playContinuousSound(final String url){
+        new Thread(new Runnable() {
+            // The wrapper thread is unnecessary, unless it blocks on the
+            // Clip finishing; see comments.
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    //read audio data from whatever source (file/classloader/etc.)
+                    InputStream audioSrc = getClass().getResourceAsStream("/audio/" + url);
+                    //add buffer for mark/reset support
+                    InputStream bufferedIn = new BufferedInputStream(audioSrc);
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+
+                    clip.open(audioStream);
+                    clip.start();
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+    }
 }
