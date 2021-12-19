49ce38b951f9d54919f3e779337119358ad13261
Alffaza <ridzkiraihanalfaza@gmail.com>
2021-12-19 15:33:31 +0700

    refactor variable name in FontLoader to match java naming convention

--- pacman/src/pacman/FontLoader.java
+++ pacman/src/pacman/FontLoader.java
@@ -5,14 +5,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class FontLoader {
-    public static Font getFontFromFile(String fontname, float fontsize) {
+    public static Font getFontFromFile(String fontName, float fontSize) {
 
         //case of error
         Font customFont = new Font("Arial",Font.PLAIN,0);
 
         try {
             //create the font to use. Specify the size!
-            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("res/fonts/" + fontname + ".ttf")).deriveFont(fontsize);
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("res/fonts/" + fontName + ".ttf")).deriveFont(fontSize);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             //register the font
             ge.registerFont(customFont);
